9c16d42d68959fa9ebd12f7ed4da6892d722a3ff
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-11-24 10:18:20 +0800

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 import Hero from "./components/hero";
-import Tech from "./components/tech";
+import Navbar from "./reusable/navbar";
 
 export default function Home() {
   return (
     <main className="overflow-y-auto snap-y snap-mandatory h-screen scroll-smooth overflow-x-hidden">
+      <Navbar />
       <div className="bg-gradient-to-r from-primary from-60% via-secondary via-0% to-secondary w-screen snap-center">
         <Hero />
       </div>
-      <div className=" snap-center flex">
-        <Tech />
-      </div>
+      <div className=" snap-center flex"></div>
       <div className="h-screen max-h-max snap-center flex"></div>
-      <div className="h-screen max-h-max snap-center  ">
-        <Hero />
-      </div>
     </main>
   );
 }
