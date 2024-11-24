99f3a83e3844e3ee3d4b5e2c3dbcabb81c631bc2
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-11-24 10:26:40 +0800

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className="overflow-y-auto snap-y snap-mandatory h-screen scroll-smooth overflow-x-hidden">
       <Navbar />
-      <div className="bg-gradient-to-r from-primary from-60% via-secondary via-0% to-secondary w-screen snap-center"></div>
 
       <div className="h-screen max-h-max snap-center flex">
         <Form></Form>
