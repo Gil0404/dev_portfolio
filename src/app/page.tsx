a1d1b655d77d18f2dc4c5ca7bc58c0fe233309a2
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-11-24 10:26:04 +0800

    ssa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
+import Navbar from "./reusable/navbar";
 import Form from "./components/form";
 
 export default function Home() {
   return (
     <main className="overflow-y-auto snap-y snap-mandatory h-screen scroll-smooth overflow-x-hidden">
+      <Navbar />
       <div className="bg-gradient-to-r from-primary from-60% via-secondary via-0% to-secondary w-screen snap-center"></div>
 
       <div className="h-screen max-h-max snap-center flex">
         <Form></Form>
       </div>
-      <div className="h-screen max-h-max snap-center  "></div>
     </main>
   );
 }
