4d25588f423cbab98e88cf6b68d1cd857dc6a211
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-12-02 16:15:27 +0800

    added svg img

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -2,32 +2,33 @@
 import React from "react";
 import { useInView, motion } from "framer-motion";
 import { useRef } from "react";
+import Aboutsvg from "../svg/aboutsvg";
 
 export default function About() {
   const container = useRef(null);
-  const inView = useInView(container, { once: false });
+  const inView = useInView(container, { once: true });
   const animate = {
     intial: {
-      opacity: 0.00001,
-      y: 2,
+      opacity: 0.5,
+      y: 20,
     },
     inView: {
       opacity: 1,
       y: 0,
       transition: {
         ease: "easeInOut",
-        duration: 0.7,
+        duration: 1,
       },
     },
   };
   return (
     <div className="max-w-max max-h-max mx-auto flex flex-row md:flex-row xs:flex-col justify-center  w-screen h-screen overflow-hidden">
-      <div className="w-1/2 h-full">
-        <h1 className="text-3xl font-bold mt-10 ml-5 text-primary xs:ml-20">
+      <div className="w-1/2 h-full md:w-1/2 xs:w-full md:h-full xs:h-1/2">
+        <h1 className="text-3xl font-bold mt-10 ml-5 text-primary xs:ml-16 xs:mt-12">
           About Me
         </h1>
         <div
-          className=" flex flex-col justify-center items-center w-full h-full"
+          className=" flex flex-col justify-center items-center w-full h-full "
           ref={container}
         >
           <motion.h1
@@ -48,7 +49,14 @@ export default function About() {
           </motion.h1>
         </div>
       </div>
-      <div className="w-1/2 h-full bg-green-400"></div>
+      <div className="w-1/2 h-full md:w-1/2 xs:w-full md:h-full xs:h-1/2">
+        <div
+          className=" flex flex-col justify-center items-center w-full h-full "
+          ref={container}
+        >
+          <Aboutsvg />
+        </div>
+      </div>
     </div>
   );
 }
