bc107f5605d1c424fc8642c13b761186bb399b8a
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-12-06 08:08:44 +0800

    added content in about

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -25,11 +25,8 @@ export default function About() {
   return (
     <div className="max-w-max max-h-max mx-auto flex flex-row md:flex-row xs:flex-col justify-center  w-screen h-screen overflow-hidden">
       <div className="w-1/2 h-full md:w-1/2 xs:w-full md:h-full xs:h-1/2">
-        <h1 className="text-3xl font-bold mt-10 ml-5 text-primary xs:ml-16 xs:mt-12">
-          About Me
-        </h1>
         <div
-          className=" flex flex-col justify-center items-center w-full h-full "
+          className=" flex flex-col justify-center items-center w-full h-full  "
           ref={container}
         >
           <motion.h1
@@ -38,15 +35,22 @@ export default function About() {
             initial="intial"
             animate={inView ? "inView" : "intial"}
           >
-            I&apos;m GILBERT GARCIA
+            About <span className="text-secondary">Me</span>
           </motion.h1>
           <motion.h1
-            className="text-md  text-[#3d155f] font-lite"
+            className="text-md  text-[#3d155f] font-lite p-2 w-1/2 md:w-1/2 xs:w-full xs:p-4"
             variants={animate}
             initial="intial"
             animate={inView ? "inView" : "intial"}
           >
-            My name
+            My name is Gilbert Garcia, and I am a dedicated and hardworking
+            developer with expertise in React, Tailwind CSS, and Firebase. I
+            also have foundational knowledge in PHP, Python, C++, Java, and
+            VB.NET. Known for my focus and commitment, I continuously strive to
+            improve my skills and deliver high-quality results. Outside of work,
+            I enjoy gaming and drawing, which allow me to unwind and express my
+            creativity. As an introverted individual, I value deep concentration
+            and enjoy working independently to solve complex problems.
           </motion.h1>
         </div>
       </div>
