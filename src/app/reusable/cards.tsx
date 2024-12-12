42817d5872388a3d8cf1acf9073e61cabcc31b55
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-12-12 15:28:14 +0800

    removing the card

--- src/app/reusable/cards.tsx
+++ src/app/reusable/cards.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { projecttype } from "../components/project";
-import Image from "next/image";
+
 export default function Card({ id, git, link, pic, name }: projecttype) {
   return (
     <div className=" h-1/3 lg:h-1/3 md:h-1/2 sm:h-1/2 xs:h-full w-full border-r-8 border-b-4 border-t-[1px] border-l-[1px] border-l-gray-300 border-t-gray-300 border-primary rounded-lg overflow-hidden flex flex-row shadow-md hover:scale-150 duration-300 bg-white">
